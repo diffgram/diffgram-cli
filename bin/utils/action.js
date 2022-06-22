da534d06a89286aa60f6a12645df1b0422582b61
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-06-21 22:42:29 -0500

    Update snippet import

--- bin/utils/action.js
+++ bin/utils/action.js
@@ -1,10 +1,11 @@
-const fs = require("fs");
 const replacer = new RegExp('NewName', 'g')
+const readModuleFile = require('../tools/readModuleFile')
 
 function action({ NewActionName }) {  
-    const buffer = fs.readFileSync("./bin/snippets/NewAction.py.txt");
+    const buffer = readModuleFile("../snippets/NewAction.py.txt");
     const template = buffer.toString()
     const newAction = template.replace(replacer, NewActionName)
+    console.log(newAction)
 }
 
 module.exports = action;
