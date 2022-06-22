c523f02a39f387a2c5994718a2a2c59a5824b0e7
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-06-21 23:08:49 -0500

    Add writing file to the action and update __init__.py to update import

--- bin/utils/action.js
+++ bin/utils/action.js
@@ -1,11 +1,21 @@
-const replacer = new RegExp('NewName', 'g')
+const fs = require("fs");
 const readModuleFile = require('../tools/readModuleFile')
+const replacer = new RegExp('NewName', 'g')
 
-function action({ NewActionName }) {  
-    const buffer = readModuleFile("../snippets/NewAction.py.txt");
-    const template = buffer.toString()
+function action({ NewActionName }) { 
+    const actionsRootDir = `${process.cwd()}/eventhandlers/action_runners/actions`
+
+    // Creating a new action file 
+    const actionFileBuffer = readModuleFile("../snippets/NewAction.py.txt");
+    const template = actionFileBuffer.toString()
     const newAction = template.replace(replacer, NewActionName)
-    console.log(newAction)
+    fs.writeFileSync(`${actionsRootDir}/${NewActionName}.py`, newAction);
+
+    // Registering action
+    const registerFileBuffer = readModuleFile("../snippets/RegisterNewAction.py.txt");
+    const registerTemplate = registerFileBuffer.toString()
+    const registrationSnippet = registerTemplate.replace(replacer, NewActionName)
+    fs.appendFileSync(`${actionsRootDir}/__init__.py`, registrationSnippet);
 }
 
 module.exports = action;
