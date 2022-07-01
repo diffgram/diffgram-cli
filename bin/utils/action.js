01b9004b9f76709b8656a9d2011c2c8562cbe565
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-07-01 12:13:36 -0500

    Add frontend

--- bin/utils/action.js
+++ bin/utils/action.js
@@ -4,6 +4,7 @@ const replacer = new RegExp('NewName', 'g')
 
 function action({ NewActionName }) { 
     const actionsRootDir = `${process.cwd()}/eventhandlers/action_runners/actions`
+    const frontendRootDir = `${process.cwd()}/frontend/src/components/action`
 
     // Creating a new action file 
     const actionFileBuffer = readModuleFile("../snippets/NewAction.py.txt");
@@ -16,6 +17,13 @@ function action({ NewActionName }) {
     const registerTemplate = registerFileBuffer.toString()
     const registrationSnippet = registerTemplate.replace(replacer, NewActionName)
     fs.appendFileSync(`${actionsRootDir}/__init__.py`, registrationSnippet);
+
+    // Create frontend component
+    const frontendFileBuffer = readModuleFile("../snippets/NewActionFrontend.txt");
+    const frontendTemplate = frontendFileBuffer.toString()
+    const frontendSnippet = frontendTemplate.replace(replacer, NewActionName)
+    fs.mkdirSync(`${frontendRootDir}/action_configurations/${NewActionName}`)
+    fs.writeFileSync(`${frontendRootDir}/action_configurations/${NewActionName}/${NewActionName}.vue`, frontendSnippet);
 }
 
 module.exports = action;
